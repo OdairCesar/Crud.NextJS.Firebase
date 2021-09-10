93991a390b37cfd5a9d5ca65d8deae5f33efc9ad
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-09-10 05:49:29 -0300

    Criando componente botão

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -71,7 +71,7 @@ export default function Tabela(props: TabelaProps){
         <table className='w-full rounded-xl overflow-hidden'>
             <thead className={`
                 text-gray-100
-                bg-gradient-to-r to-purple-500 to-purple-800
+                bg-gradient-to-r from-purple-500 to-purple-800
             `}>
                 {renderizarCabecalho()}
             </thead>
